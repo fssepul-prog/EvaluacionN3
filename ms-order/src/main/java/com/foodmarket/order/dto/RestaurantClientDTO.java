cfff0e1cfd20f7fc730f15307bca17045ece50a7
fssepul-prog <felipeandressepul2025@gmail.com>
2026-06-23 11:55:27 -0400

    Delete ms-order directory
    carga erronea

--- ms-order/src/main/java/com/foodmarket/order/dto/RestaurantClientDTO.java
+++
@@ -1,4 +0,0 @@
-package com.foodmarket.order.dto;
-import lombok.*;
-@Data @NoArgsConstructor @AllArgsConstructor
-public class RestaurantClientDTO { private Long id; private String name; private String zone; private String status; }
